f1536c220c286f9ffd2d9ff5d51ee5f73f0edef4
吴雨康 <wuyukang@sofunny.com>
2025-12-15 14:58:58 +0800

    feat: Ensure AUTH_SECRET and trustHost are set in NextAuth configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,8 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  secret: process.env.AUTH_SECRET,
+  trustHost: true,
   providers: [
     GitHub({
       authorization: {
